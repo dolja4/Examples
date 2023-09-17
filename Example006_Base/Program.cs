51ff28fb7817a920e47af505c6fd025d2b8161a8
dolja <hpuser230820@oulook.com>
2023-09-17 20:22:09 +0300

    add Example 006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int A = 3;
+int B = 10;
+int C = 6;
+int D = 8;
+int E = 4;
+
+int max = A;
+
+if (B > max) max = B;
+if (C > max) max = C;
+if (D > max) max = D;
+if (E > max) max = E;
+
+Console.Write("max =");
+Console.WriteLine(max);
